9257619b84d9e5023a4365cc62c5ca39c1357f24
li-xiaoying <lixiaoyi@oregonstate.edu>
2020-11-12 18:47:29 -0800

    Update utils.js

--- src/main/webapp/scripts/utils.js
+++ src/main/webapp/scripts/utils.js
@@ -1,18 +1,18 @@
 //show button
-function hideElement(element){
+function hideElement(element) {
 	element.style.display = 'none';
 }
 //hide button
-function showElement(element, style){
+function showElement(element, style) {
 	var displayStyle = style ? style : 'block';
-    element.style.display = displayStyle;
+	element.style.display = displayStyle;
 }
 
 function showLoadingMessage(msg) {
-    var itemList = document.querySelector('#item-list');
-    itemList.innerHTML = '<p class="notice"><i class="fa fa-spinner fa-spin"></i> ' +
-      msg + '</p>';
- }
+	var itemList = document.querySelector('#item-list');
+	itemList.innerHTML = '<p class="notice"><i class="fa fa-spinner fa-spin"></i> ' +
+		msg + '</p>';
+}
 
 function showWarningMessage(msg) {
 	var itemList = document.querySelector('#item-list');
@@ -20,21 +20,81 @@ function showWarningMessage(msg) {
 }
 
 function showErrorMessage(msg) {
-    var itemList = document.querySelector('#item-list');
-    itemList.innerHTML = '<p class="notice"><i class="fa fa-exclamation-circle"></i> ' + msg + '</p>';
+	var itemList = document.querySelector('#item-list');
+	itemList.innerHTML = '<p class="notice"><i class="fa fa-exclamation-circle"></i> ' + msg + '</p>';
 }
 
 /**
- * List recommendation items based on the data received
+ * List nearby items based on the data received
  * @params items - An array of item JSON objects
  */
-function listItems(items){
+function listItems(items) {
 	var itemList = document.querySelector('#item-list');
 	itemList.innerHTML = ''; //clear current results;
-	
-	for(var i = 0; i < items.length; i++){
+
+	for (var i = 0; i < items.length; i++) {
 		addItem(itemList, items[i]);
 	}
+}
+
+/**
+ * List trial detail information based on the data received
+ * @params detail - An array of detail JSON objects
+ */
+function listDetails(trail) {
+	var itemList = document.querySelector('#item-list');
+	itemList.innerHTML = ''; // clear current results
+	addDetails(itemList, trail);
+}
+
+//PLEASE CHANGE HERE!!!!
+function addDetails(itemList, trail) {
+	//create the <li> tag and specify the id and class attributes
+	var li = $create('li', {
+		id: trail.id,
+		className: 'trailDetail'
+	});
+	//set the data attributes <li data-item_id="xxxxx">
+	li.dataset.trail_id = trail.id;
+
+	//trail image
+	if (trail.imgSmallMed) {
+		li.appendChild($create('img', {
+			src: trail.imgSmallMed
+		}));
+	} else {
+		li.appendChild($create('img', {
+			src: 'https://via.placeholder.com/100'
+		}));
+	}
+
+	//trail name
+	var trailName = $create('a', {
+		className: 'trail-name',
+	});
+	trailName.innerHTML = trail.name;
+	li.appendChild(trailName);
+
+	//section
+	var section = $create('div');
+	//trail length
+	var trailLength = $create('a', {
+		className: 'trail-length',
+	});
+	trailLength.innerHTML = trail.length;
+	li.appendChild(trailLength);
+
+	//section
+	var section = $create('div');
+
+	// address
+	var address = $create('p', {
+		className: 'trail-address'
+	});
+	address.innerHTML = trail.location;
+	li.appendChild(address);
+
+	itemList.appendChild(li);
 }
 
 /**
@@ -48,57 +108,60 @@ function listItems(items){
  * </div>
  * <p class="item-address">Superior, Colorado</p>
  */
-function addItem(itemList, item){
-	var item_id = item.id;
-	
+function addItem(itemList, item) {
+	var item_id = 'item-' + item.id;
+
 	//create the <li> tag and specify the id and class attributes
 	var li = $create('li', {
-		id: 'item-' + item_id,
-		className: 'item'
+		id: item_id,
+		className: 'item',
+		value: item.id
 	});
-	
+
 	//set the data attributes <li data-item_id="xxxxx">
-	li.dataset.item_id = item_id;
+	li.dataset.item_id = item.id;
 
 	//item image
-	if(item.imgSmallMed){
+	if (item.imgSmallMed) {
 		li.appendChild($create('img', {
-			src : item.imgSmallMed
+			src: item.imgSmallMed
 		}));
-	} else{
+	} else {
 		li.appendChild($create('img', {
-			src : 'https://via.placeholder.com/100'
+			src: 'https://via.placeholder.com/100'
 		}));
 	}
-	
+
 	//section
 	var section = $create('div');
-	
+
 	//title
 	var title = $create('a', {
-		className : 'item-title',
-		href : "http://localhost:8080/HikingEveryday/", //need to be changed
-		target : '_blank'
+		className: 'item-link',
+		href: '#',
 	});
 	title.innerHTML = item.name;
-	section.appendChild(title);
-	
+	title.onclick = function () {
+		loadTrailInfo(item);
+	}
+	section.appendChild(title)
+
 	// trial name
-//	var name = $create('p', {
-//		className : 'item-name'
-//	});
-//	name.innerHTML = item.name;
-//	section.appendChild(name);
-	
+	//	var name = $create('p', {
+	//		className : 'item-name'
+	//	});
+	//	name.innerHTML = item.name;
+	//	section.appendChild(name);
+
 	li.appendChild(section);
-	
+
 	// address
 	var address = $create('p', {
-		className : 'item-address'
+		className: 'item-address'
 	});
 	address.innerHTML = item.location;
 	li.appendChild(address);
-	
+
 	itemList.appendChild(li);
 }
 
@@ -108,10 +171,10 @@ function addItem(itemList, item){
  * @param options
  * @returns {element}
  */
-function $create(tag, options){
+function $create(tag, options) {
 	var element = document.createElement(tag);
-	for(var key in options){
-		if(options.hasOwnProperty(key)){
+	for (var key in options) {
+		if (options.hasOwnProperty(key)) {
 			element[key] = options[key];
 		}
 	}
@@ -122,11 +185,11 @@ function $create(tag, options){
  * A helper function that makes a navigation button active
  * @param btnId - The id of navigation button
  */
-function activeBtn(btnId){
+function activeBtn(btnId) {
 	var btns = document.querySelectorAll('.main-nav-btn');
-	
+
 	//de-active all navigation buttons
-	for(var i = 0; i < btns.length; i++){
+	for (var i = 0; i < btns.length; i++) {
 		btns[i].className = btns[i].className.replace(/\bactive\b/, '');
 	}
 	//active the one that has id = btnId
@@ -142,29 +205,29 @@ function activeBtn(btnId){
  *@param successCallback - Successful callback function
  *@param errorCallback - Error callback function
  */
-function ajax(method, url, data, successCallback, errorCallback){
+function ajax(method, url, data, successCallback, errorCallback) {
 	//step1: create an XMLHttpRequest object
-	var xhr = new XMLHttpRequest(); 
-	
+	var xhr = new XMLHttpRequest();
+
 	//step2: send the request to the server
-	xhr.open(method, url, true); 
+	xhr.open(method, url, true);
 	console.log(method);
-	xhr.onload = function(){
-		if(xhr.status === 200){
+	xhr.onload = function () {
+		if (xhr.status === 200) {
 			successCallback(xhr.responseText);
-		} else{
+		} else {
 			errorCallback();
 		}
 	};
-	
-	xhr.onerror = function(){
+
+	xhr.onerror = function () {
 		console.log("The requext couldn't be completed.");
 		errorCallback();
 	};
-	
-	if(data === null){
+
+	if (data === null) {
 		xhr.send();
-	} else{
+	} else {
 		xhr.setRequestHeader("Content-Type", "application/json;charset=utf-8");
 		xhr.send(data);
 	}
