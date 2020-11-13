d8eae881a462218d992df0838f2a67a21c0e56cd
Zhixiang Ye <yezhixiang88@gmail.com>
2020-11-13 17:28:08 -0500

    add show nearby trails by address, buttons to link to homepage, profile page, and trail info page

--- src/main/webapp/scripts/main.js
+++ src/main/webapp/scripts/main.js
@@ -6,7 +6,7 @@ var lon = -105.2519;
 // var trailId;
 init();
 
-//entry
+// entry
 function init(){
 	//add search by zip code feature
 	document.querySelector('#search-btn').addEventListener('click', initMap);
@@ -14,7 +14,7 @@ function init(){
 	document.querySelector('#home-btn').addEventListener('click', showHomePage);
 };
 
-//initMap function
+// initMap function
 function initMap() {
 	var profileForm = document.querySelector('#profile-form');
 	hideElement(profileForm);
@@ -23,75 +23,66 @@ function initMap() {
       geocodeAddress(geocoder);
     });
 	
-	//display loading message
+	// display loading message
     showErrorMessage('No nearby trials. Please enter zipcode or city name.');
   }
 
 
-//fetch location 
-function iniGeolocation(){
-	if(navigator.geolocation){
-		navigator.geolocation.getCurrentPosition(
-		onPositionUpdated, onLoadPositionFailed, {maximumAge: 60000});
-	} else{
-		onLoadPositionFailed();
-	}
-}
-
-//update position
-function onPositionUpdated(position){
-	lat = position.coords.latitude;
-	lng = position.coords.longitude;
-	console.log(lat, lng);
-	
-	loadNearbyItems(); //load nearby items
-}
-
-//failed to fetch position (user clicked no)
-function onLoadPositionFailed(){
-	console.warn('navigator.geolocation is not avaliable');
-	getLocationFromIP(); 
-}
-
-//get location from ip
-function getLocationFromIP(){
-	var url = 'http://ipinfo.io/json';
-	var data = null;
-	ajax('GET', url, data, function(res){
-		var result = JSON.parse(res);
-		if('loc' in result){
-			var loc = result.loc.split(',');
-			lat = loc[0];
-			lng = loc[1];
-			loadNearbyItems();
-		} else{
-			console.warn('Getting location by IP failed.');
-		}
-		loadNearbyItems();
-	});	
-}
-
-//initMap function
-function initMap() {
-    const geocoder = new google.maps.Geocoder();
-    document.getElementById("search-btn").addEventListener("click", () => {
-      geocodeAddress(geocoder);
-    });
-  }
-
-//geocodeAddress function
+// geocodeAddress function
 function geocodeAddress(geocoder) {
 	var address = document.querySelector('#address').value;
     geocoder.geocode({ address: address }, (results, status) => {
       if (status === "OK") {
         
         lat = results[0].geometry.location.lat();
-        lon = results[0].geometry.location.lng();
+        lng = results[0].geometry.location.lng();
         console.log('In google maps api');
         console.log(lat, lng);
-        loadNearbyItems(); //load nearby items
+        loadNearbyItems(); // load nearby items
       } else {
         alert("Geocode was not successful for the following reason: " + status);
       }
     });
  }
+
+// function to list user profile form
+function showProfileForm(){
+	console.log("in show profile form function");
+	var searchForm = document.querySelector('#search-form');
+    var itemNav = document.querySelector('#item-nav');
+    var itemList = document.querySelector('#item-list');
+    var avatar = document.querySelector('#avatar');
+    var welcomeMsg = document.querySelector('#welcome-msg');
+    var profileForm = document.querySelector('#profile-form');
+    
+    hideElement(searchForm);
+    hideElement(itemNav);
+    hideElement(itemList);
+    hideElement(avatar);
+    hideElement(welcomeMsg);
+    showElement(profileForm);
+}
+
+// function to go back to homepage 
+function showHomePage(){
+	var searchForm = document.querySelector('#search-form');
+    var itemNav = document.querySelector('#item-nav');
+    var itemList = document.querySelector('#item-list');
+    var avatar = document.querySelector('#avatar');
+    var welcomeMsg = document.querySelector('#welcome-msg');
+    var profileForm = document.querySelector('#profile-form');
+    
+    showElement(searchForm);
+    showElement(itemNav);
+    showElement(itemList);
+    showElement(avatar);
+    showElement(welcomeMsg);
+    hideElement(profileForm);
+    loadNearbyItems();
+}
+
+
+// function to show the trailInfo page
+function showTrailInfo(){
+	loadTrailInfo();
+}
