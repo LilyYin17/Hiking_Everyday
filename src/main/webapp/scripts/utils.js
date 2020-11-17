ca3c1aa9ed5fc90fef65892117be8ca5a2440076
Liying Yin <yinliying53@gmail.com>
2020-11-17 12:09:53 -0800

    add hideModal to utils.js

--- src/main/webapp/scripts/utils.js
+++ src/main/webapp/scripts/utils.js
@@ -40,6 +40,11 @@ function showModal() {
 	});
 }
 
+function hideModal() {
+	var modal = document.getElementById("myModal");
+	hideElement(modal);
+}
+
 /**
  * List nearby items based on the data received
  * @params items - An array of item JSON objects
